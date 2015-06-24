460a50696df9cff0627ec6d8a137b21c855cb1be
jpsymfony <jpsymfony@free.fr>
2015-06-24 12:52:08 +0200

    typo

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -41,7 +41,7 @@ class UserController extends Controller
         ]);
     }
 
-        /**
+    /**
      * @return \AppBundle\Form\Handler\FormHandlerInterface
      */
     protected function getPasswordFormHandler()
