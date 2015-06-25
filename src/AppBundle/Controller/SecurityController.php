3ac99468990e3a113f99a16184ee5ef87b65a06c
jpsymfony <jpsymfony@free.fr>
2015-06-25 10:16:27 +0200

    code moved to other controllers

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -12,10 +12,6 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Symfony\Component\HttpFoundation\Request;
-use AppBundle\Form\Type\RegistrationType;
-use AppBundle\User\Registration\Registration;
 
 /**
  * Controller used to manage the application security.
@@ -41,31 +37,6 @@ class SecurityController extends Controller
                 // last authentication error (if any)
                 'error' => $helper->getLastAuthenticationError(),
         ));
-    }
-
-    /**
-     * @Route("/register", name="security_register_form")
-     * @Method({"GET", "POST"})
-     */
-    public function registerAction(Request $request)
-    {
-        $form = $this->createForm(new RegistrationType(), new Registration());
-
-        if ($this->getRegistrationFormHandler()->handle($form, $request)) {
-            return $this->redirect($this->generateUrl('security_login_form'));
-        }
-
-        return $this->render('security/register.html.twig', [
-                'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @return \AppBundle\Form\Handler\FormHandlerInterface
-     */
-    protected function getRegistrationFormHandler()
-    {
-        return $this->container->get('app.registration.handler');
     }
 
     /**
--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -2,11 +2,9 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\User\Password\ChangePassword;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/account")
@@ -20,32 +18,5 @@ class UserController extends Controller
     public function dashboardAction()
     {
         return $this->render('user/dashboard.html.twig');
-    }
-
-    /**
-     * @Route("/change-password", name="change_password")
-     * @Method("GET|POST")
-     */
-    public function changePasswordAction(Request $request)
-    {
-        $data = new ChangePassword($this->getUser());
-        $form = $this->createForm('change_password', $data);
-
-        if ($this->getPasswordFormHandler()->handle($form, $request)) {
-            $this->addFlash('success', 'Password has been changed successfully.');
-            return $this->redirect($this->generateUrl('user_dashboard'));
-        }
-
-        return $this->render('user/password.html.twig', [
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @return \AppBundle\Form\Handler\FormHandlerInterface
-     */
-    protected function getPasswordFormHandler()
-    {
-        return $this->container->get('app.password.handler');
     }
 }
